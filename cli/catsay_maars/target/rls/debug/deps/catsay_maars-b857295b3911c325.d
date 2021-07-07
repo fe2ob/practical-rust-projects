cb37d9091770394a1b82bc0986cfe280eecbd1c3
fe2ob <maars202@gmail.com>
2021-07-07 18:26:10 +0800

    started cli with own folder catsay_maars

---
+++ cli/catsay_maars/target/rls/debug/deps/catsay_maars-b857295b3911c325.d
@@ -0,0 +1,5 @@
+/Users/maars/Desktop/fe2ob/practical-rust-projects/cli/catsay_maars/target/rls/debug/deps/catsay_maars-b857295b3911c325.rmeta: src/main.rs
+
+/Users/maars/Desktop/fe2ob/practical-rust-projects/cli/catsay_maars/target/rls/debug/deps/catsay_maars-b857295b3911c325.d: src/main.rs
+
+src/main.rs:
---
+++ cli/catsay_maars/target/rls/debug/deps/catsay_maars-fb6d35da8f2dac45.d
@@ -0,0 +1,5 @@
+/Users/maars/Desktop/fe2ob/practical-rust-projects/cli/catsay_maars/target/rls/debug/deps/catsay_maars-fb6d35da8f2dac45.rmeta: src/main.rs
+
+/Users/maars/Desktop/fe2ob/practical-rust-projects/cli/catsay_maars/target/rls/debug/deps/catsay_maars-fb6d35da8f2dac45.d: src/main.rs
+
+src/main.rs:
